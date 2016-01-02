4497f5e6cc9dc984c1bfa02596dfc2f746fb6064
JP <jpsala@gmail.com>
2016-01-02 19:30:26 -0300

    varios

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,6 @@
+sleep 20s
+killall conky
+cd "/home/jpsala/.conky/Gotham"
+conky -c "/home/jpsala/.conky/Gotham/Gotham" &
+cd "/home/jpsala/.conky/TeejeeTech"
+conky -c "/home/jpsala/.conky/TeejeeTech/Process Panel" &
